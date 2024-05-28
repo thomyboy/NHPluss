b2cd82d8284b87827be116e3a481516b4e45b10f
fynnr3 <144106437+fynnr3@users.noreply.github.com>
2024-05-28 12:40:44 +0200

    Ändern der Datenbank
    Das Datenbank Schema wurde in "\NHPluss\src\main\java\de\hitec\nhplus\utils\SetUpDB.java" auf den Sollzustand angepasst

--- src/main/java/de/hitec/nhplus/utils/SetUpDB.java
+++ src/main/java/de/hitec/nhplus/utils/SetUpDB.java
@@ -29,8 +29,10 @@ public class SetUpDB {
     public static void setUpDb() {
         Connection connection = ConnectionBuilder.getConnection();
         SetUpDB.wipeDb(connection);
+        SetUpDB.setUpTableRoom(connection);
         SetUpDB.setUpTablePatient(connection);
-        SetUpDB.setUpTableTreatment(connection);
+        SetUpDB.setUpTableEmployee(connection);
+        SetUpDB.setUpTableUser(connection);
         SetUpDB.setUpPatients();
         SetUpDB.setUpTreatments();
     }
@@ -40,22 +42,37 @@ public class SetUpDB {
      */
     public static void wipeDb(Connection connection) {
         try (Statement statement = connection.createStatement()) {
-            statement.execute("DROP TABLE patient");
-            statement.execute("DROP TABLE treatment");
+            statement.execute("DROP TABLE IF EXISTS patient");
+            statement.execute("DROP TABLE IF EXISTS treatment");
+            statement.execute("DROP TABLE IF EXISTS room");
+            statement.execute("DROP TABLE IF EXISTS employee");
         } catch (SQLException exception) {
             System.out.println(exception.getMessage());
         }
     }
 
+    private static void setUpTableRoom(Connection connection) {
+        final String SQL = "CREATE TABLE IF NOT EXISTS room (" +
+                "   roomID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "   roomName TEXT NOT NULL " +
+                " " +
+                ");";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(SQL);
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
     private static void setUpTablePatient(Connection connection) {
         final String SQL = "CREATE TABLE IF NOT EXISTS patient (" +
-                "   pid INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "   patientID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "   roomID INTEGER NOT NULL, " +
                 "   firstname TEXT NOT NULL, " +
                 "   surname TEXT NOT NULL, " +
                 "   dateOfBirth TEXT NOT NULL, " +
                 "   carelevel TEXT NOT NULL, " +
-                "   roomnumber TEXT NOT NULL, " +
-                "   assets TEXt NOT NULL" +
+                "   FOREIGN KEY (roomID) REFERENCES room (roomID) " +
+                " " +
                 ");";
         try (Statement statement = connection.createStatement()) {
             statement.execute(SQL);
@@ -66,14 +83,17 @@ public class SetUpDB {
 
     private static void setUpTableTreatment(Connection connection) {
         final String SQL = "CREATE TABLE IF NOT EXISTS treatment (" +
-                "   tid INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "   pid INTEGER NOT NULL, " +
-                "   treatment_date TEXT NOT NULL, " +
+                "   treatmentID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "   patientID INTEGER NOT NULL, " +
+                "   employeeID INTEGER NOT NULL, " +
+                "   treatment_date DATE NOT NULL, " +
                 "   begin TEXT NOT NULL, " +
                 "   end TEXT NOT NULL, " +
                 "   description TEXT NOT NULL, " +
-                "   remark TEXT NOT NULL," +
-                "   FOREIGN KEY (pid) REFERENCES patient (pid) ON DELETE CASCADE" +
+                "   remark TEXT NOT NULL, " +
+                "   state TEXT NOT NULL, " +
+                "   FOREIGN KEY (patientID) REFERENCES patient (patientID) ON DELETE CASCADE, " +
+                "   FOREIGN KEY (employeeID) REFERENCES employee (employeeID) ON DELETE CASCADE " +
                 " " +
                 ");";
 
@@ -84,6 +104,37 @@ public class SetUpDB {
         }
     }
 
+    private static void setUpTableEmployee(Connection connection) {
+        final String SQL = "CREATE TABLE IF NOT EXISTS employee (" +
+                "   employeeID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "   firstname TEXT NOT NULL, " +
+                "   surname TEXT NOT NULL, " +
+                "   role TEXT NOT NULL, " +
+                "   status TEXT NOT NULL" +
+                " " +
+                ");";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(SQL);
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    private static void setUpTableUser(Connection connection) {
+        final String SQL = "CREATE TABLE IF NOT EXISTS user (" +
+                "   userID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "   employeeID INTEGER NOT NULL, " +
+                "   userName TEXT NOT NULL, " +
+                "   userPassword TEXT NOT NULL, " +
+                "   FOREIGN KEY (employeeID) REFERENCES employee (employeeID) ON DELETE CASCADE " +
+                " " +
+                ");";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(SQL);
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
 
     private static void setUpPatients() {
         try {
