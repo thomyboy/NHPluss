d15c8fc84615c881c7b6a0066930969552408700
thomboy <thomas432@schule.bremen.de>
2024-05-31 20:06:47 +0200

    - began to implement combobox in NewTreatmentController.java, working towards DB connection now

--- src/main/java/de/hitec/nhplus/controller/NewTreatmentController.java
+++ src/main/java/de/hitec/nhplus/controller/NewTreatmentController.java
@@ -1,8 +1,13 @@
 package de.hitec.nhplus.controller;
 
 import de.hitec.nhplus.datastorage.DaoFactory;
+import de.hitec.nhplus.datastorage.EmployeeDao;
+import de.hitec.nhplus.datastorage.PatientDao;
 import de.hitec.nhplus.datastorage.TreatmentDao;
+import de.hitec.nhplus.model.Employee;
 import javafx.beans.value.ChangeListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
@@ -14,6 +19,7 @@ import javafx.util.StringConverter;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class NewTreatmentController {
 
@@ -41,9 +47,19 @@ public class NewTreatmentController {
     @FXML
     private Button buttonAdd;
 
+    @FXML
+    private ComboBox<String> comboBoxEmployeeSelection;
+
     private AllTreatmentController controller;
     private Patient patient;
     private Stage stage;
+    private TreatmentDao dao;
+    private ArrayList<Employee> employeeList;
+    private final ObservableList<String> employeeSelection = FXCollections.observableArrayList();
+    private final ObservableList<Treatment> treatments = FXCollections.observableArrayList();
+
+
+
 
     public void initialize(AllTreatmentController controller, Stage stage, Patient patient) {
         this.controller= controller;
@@ -97,6 +113,53 @@ public class NewTreatmentController {
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
+    }
+
+    private void createComboBoxData() {
+        EmployeeDao dao = DaoFactory.getDaoFactory().createEmployeeDAO();
+        try {
+            employeeList = (ArrayList<Employee>) dao.readAll();
+            this.employeeSelection.add("alle");
+            for (Employee employee: employeeList) {
+                this.employeeSelection.add(patient.getSurname());
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+
+    @FXML
+    public void handleComboBox() {
+        String selectedPatient = this.comboBoxEmployeeSelection.getSelectionModel().getSelectedItem();
+        this.treatments.clear();
+        this.dao = DaoFactory.getDaoFactory().createTreatmentDao();
+
+        if (selectedPatient.equals("alle")) {
+            try {
+                this.treatments.addAll(this.dao.readAll());
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+        }
+
+        Employee employee = searchInList(selectedPatient);
+        if (patient !=null) {
+            try {
+                this.treatments.addAll(this.dao.readTreatmentsByPid(patient.getPid()));
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+
+    private Employee searchInList(String surname) {
+        for (Employee employee : this.employeeList) {
+            if (patient.getSurname().equals(surname)) {
+                return employee;
+            }
+        }
+        return null;
     }
 
     @FXML
